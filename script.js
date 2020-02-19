181a3ef52153dfc3b1358beca863eda3a2f4f078
rajurocker <rajurocker1999@gmail.com>
2020-02-19 15:17:55 +1300

    Made changes to it

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('npm-grunt'); // to check if script is working
+
+$(document).ready(function(){
+
+  //to check if jquery node_module is working
+  $('body').click(function(){
+    $(this).css('background','cyan');
+  });
+});
